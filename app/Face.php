5c96f66f352d2681bd3f0f0f74e11c11fc75329d
Ross Lindsay <ross@rosslindsay.com>
2018-04-13 17:08:32 -0400

    Add slug and tag code

---
+++ app/Face.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+use Spatie\Sluggable\HasSlug;
+use Spatie\Sluggable\SlugOptions;
+use Spatie\Tags\HasTags;
+
+class Face extends Model
+{
+    use HasSlug, HasTags;
+
+    /**
+     * Get the options for generating the slug.
+     */
+    public function getSlugOptions() : SlugOptions
+    {
+        return SlugOptions::create()
+            ->generateSlugsFrom('title')
+            ->saveSlugsTo('slug');
+    }
+}
