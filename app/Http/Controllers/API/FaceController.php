c4542aacb1e61c7c4e8ad0a870b6e9b841fc4d91
Ross Lindsay <ross@rosslindsay.com>
2018-04-25 17:12:08 -0400

    Started back-end with base controllers and routes

---
+++ app/Http/Controllers/API/FaceController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Face;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class FaceController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Face  $face
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Face $face)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Face  $face
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Face $face)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Face  $face
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Face $face)
+    {
+        //
+    }
+}
